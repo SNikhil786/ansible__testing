bd55fda5c737e8e48c7230d333066230c8d556fd
SNikhil2000 <sutrayenikhil2000@gmail.com>
2025-05-03 12:39:31 +0530

    added ansible files...

--- 1.provider.tf
+++ 1.provider.tf
@@ -11,10 +11,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "workspacesbucket01"
-    key            = "Ansible.tfstate"
-    region         = "us-east-1"
+    bucket = "nikhil78699999"
+    key    = "Ansible.tfstate"
+    region = "us-east-1"
     # dynamodb_table = "-terraform-locks"
-    encrypt        = true
+    encrypt = true
   }
 }
