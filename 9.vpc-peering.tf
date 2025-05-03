7fea68fef4ac112d813fa1c9b7dab53326764447
SNikhil2000 <sutrayenikhil2000@gmail.com>
2025-05-03 14:30:55 +0530

    updated vpc peering

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -3,7 +3,8 @@ data "aws_vpc" "ansible_vpc" {
 }
 
 data "aws_route_table" "ansible_vpc_rt" {
-  subnet_id = "subnet-006b058e572dbc43e"
+  # subnet_id = "subnet-006b058e572dbc43e"
+  route_table_id = "rtb-073a833fffdcb7890"
   #If subnet_id giving errors use route table id as below
   #route_table_id = data.aws_route_table.ansible_vpc_rt.id
 }
